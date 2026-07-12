5496bd310358ca2e84bc41dab39ebd25c57090a2
Gaohar Imran <gaoharimran29@gmail.com>
2026-07-12 21:38:14 +0530

    Delete extension directory

--- extension/popup.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("test-btn");
-const status = document.getElementById("status");
-
-button.addEventListener("click", () => {
-    status.textContent = "✅ Extension is working!";
-});
